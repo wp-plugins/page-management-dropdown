e51d69e70febcf2f480fa81e9af320a3891511dd
jaschaephraim <jaschaephraim@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-09 23:12:49 +0000

    Upating to version 2.5, minor bug fix and updated screenshot
    git-svn-id: https://plugins.svn.wordpress.org/page-management-dropdown/tags/2.5@835791 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- page-management-dropdown.php
+++ page-management-dropdown.php
@@ -5,7 +5,7 @@
 Plugin Name: Page Management Dropdown
 Plugin URI: http://jaschaephraim.com/wordpress/
 Description: Adds a link to edit each individual page to the Pages admin menu.
-Version: 2.4
+Version: 2.5
 Author: Jascha Ephraim
 Author URI: http://jaschaephraim.com/
 License: GPL3
@@ -27,23 +27,26 @@ License: GPL3
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-add_action('admin_menu', 'jeml_page_management_dropdown');
+add_action( 'admin_menu', 'jeml_page_management_dropdown' );
 
 function jeml_page_management_dropdown() {
 	global $submenu, $wpdb;
-	$pages = $wpdb->get_results("SELECT ID, post_title, post_parent FROM $wpdb->posts WHERE post_type = 'page' AND post_status = 'publish' ORDER BY menu_order ASC, post_title ASC");
-	$indexed_pages = get_page_hierarchy($pages);
-	foreach ($pages as $page) {
-		$indexed_pages[$page->ID] = $page;
-	}
-	foreach ($indexed_pages as $page) {
+	$pages = $wpdb->get_results( "SELECT ID, post_title, post_name, post_parent FROM $wpdb->posts WHERE post_type = 'page' AND post_status = 'publish' ORDER BY menu_order ASC, post_title ASC" );
+	$indexed_pages = get_page_hierarchy( $pages );
+	
+  foreach ( $pages as $page )
+		$indexed_pages[ $page->ID ] = $page;
+	
+  foreach ( $indexed_pages as $page ) {
 		$indent = '';
 		$parent = $page->post_parent;
-		while ($parent != 0) {
-			$indent .= '&nbsp;&nbsp;';
-			$parent = $indexed_pages[$parent]->post_parent;
+
+		while ( $parent != 0 ) {
+      $indent .= '&nbsp;&nbsp;&nbsp;';
+			$parent = $indexed_pages[ $parent ]->post_parent;
 		}
-		$submenu['edit.php?post_type=page'][] = array($indent.$page->post_title, 'edit_pages', 'post.php?action=edit&post='.$page->ID);
+
+		$submenu[ 'edit.php?post_type=page' ][] = array( $indent . $page->post_title, 'edit_pages', 'post.php?action=edit&post='.$page->ID );
 	}
 }
 
